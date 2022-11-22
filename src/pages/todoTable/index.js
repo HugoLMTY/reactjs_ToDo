9e4b02d595c74ca6c24020ecd7c295fc1f599dab
Hugo <hugo.lm2707@gmail.com>
2022-11-22 15:46:32 +0100

    dnd kinda working

--- src/pages/todoTable/index.js
+++ src/pages/todoTable/index.js
@@ -1,77 +1,210 @@
 //? Native
 import React, { useState, useEffect } from 'react';
 
+//? Imports
+import './style.css';
+
 //? Packages
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
-//? Imports
-import './style.css'
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+const swal = withReactContent(Swal);
 
 
-
-const swal = withReactContent(Swal)
 /*
-		* List Model
-		? rows: [ 
-		?		{ 
-		?			name: "Row name", 
-		?			tickets: [ Ticket,...], 
-		?			position: 1 | 2 | ... | h 
-		?		} 
-		?	]
+	* List Model
+	? columns: [ 
+	?		{ 
+	?			id: 		Number 	[ 20 | 21 | 22 ]
+	?			name: 		String 	[ Column name ], 
+	?			tickets: 	Array 	[ Ticket,...], 
+	?			position: 	Number 	[ from 0 to n ]
+	?		} 
+	?	]
 
-		* Ticket Model
-		? id: 			Number [ from 0 to n ]
-		? title: 		String [ New ticket ]
-		? desc: 		String [ No description ]
-		? status: 		String [ Created | In Progress | Done ]
+	* Ticket Model
+	? id: 			Number [ 10 | 11 | 12 ]
+	? title: 		String [ New ticket ]
+	? desc: 		String [ No description ]
+	? status: 		String [ Created | In Progress | Done ]
+	? position: 	Number [ from 0 to n ]
 
-	*/
+*/
+
+/*
+	TODO
+	* Ajouter des users + membres sur les cartes
+	* Ajouter des labels sur les cartes
+	* Ajouter des dates sur les cartes
+*/
 
 const TodoList = () => {
-
-	//#region --------------- DEFINITIONS ---------------
-	const [ List, setList ] = useState({
-		rows: [
-			{ name: "Todo", 		tickets: [ { id: 10, title: "Title todo", 	 	desc: "Desc todo", 		status: "Created"} ], position: 0 },
-			{ name: "In Progress", 	tickets: [ { id: 11, title: "Title progress", 	desc: "Desc progress", 	status: "Created"} ], position: 1 },
-			{ name: "Done", 		tickets: [ { id: 12, title: "Title done", 		desc: "Desc done", 		status: "Created"} ], position: 2 }
-		]
-	})
+	//#region --------------- STATES ---------------
+	const [List, setList] = useState({
+		columns: [
+			{
+				id: 20,
+				name: 'Todo',
+				tickets: [
+					{
+						id: 10,
+						title: 'Title todo',
+						desc: 'Desc todo',
+						status: 'Created',
+						position: 0
+					},
+				],
+				position: 0,
+				expanded: true,
+			},
+			{
+				id: 21,
+				name: 'In Progress',
+				tickets: [
+					{
+						id: 11,
+						title: 'Title progress',
+						desc: 'Desc progress',
+						status: 'Created',
+						position: 0
+					},
+				],
+				position: 1,
+				expanded: true,
+			},
+			{
+				id: 22,
+				name: 'Done',
+				tickets: [
+					{
+						id: 12,
+						title: 'Title done',
+						desc: 'Desc done',
+						status: 'Created',
+						position: 0
+					},
+				],
+				position: 2,
+				expanded: true,
+			},
+		],
+	});
 	useEffect(() => {
-		const sorted = List.rows.sort((a, b) => a.position - b.position)
-		if (sorted !== List.rows) 
-			setList({ rows: sorted })
-	}, [ List ])
+		const sorted = List.columns.sort((a, b) => a.position - b.position);
+		if (sorted !== List.columns) setList({ columns: sorted });
+	}, [List]);
 	//#endregion
 
-	//#region --------------- ACTIONS ---------------
-	const changeRow = (ticketId, rowPos, action) => {
-		const row = List.rows[rowPos]
-		const ticket = row.tickets.find(ticket => ticket.id === ticketId)
+	//#region --------------- ACTIONS ---------------	
 
-		if (action === '+') {
-			if (rowPos < List.rows.length - 1) {
-				const nextRow = List.rows[rowPos + 1]
-				nextRow.tickets.push(ticket)
-				row.tickets = row.tickets.filter(ticket => ticket.id !== ticketId)
-			}
-		} else if (action === '-') {
-			if (rowPos > 0) {
-				const prevRow = List.rows[rowPos - 1]
-				prevRow.tickets.push(ticket)
-				row.tickets = row.tickets.filter(ticket => ticket.id !== ticketId)
-			}
-		}
+	//? ------------------ COLUMNS ------------------
+	const createColumn = async () => {
+		const name = await swal.fire({
+			title: 'Create new column',
+			input: 'text',
+			inputValue: 'New column',
+			inputPlaceholder: '',
+			showCancelButton: true,
+			confirmButtonText: 'Create',
+			preConfirm: (name) => {
+				if (name === '') {
+					swal.showValidationMessage(`Please enter a name`);
+				}
+				return name;
+			},
+		});
+		if (!name.isConfirmed) return;
+		if (name.value.trim().length <= 3) return swalError('Name must be more than 3 characters long');
 
-		setList({ rows: List.rows })
-	}
+		const column = {
+			id: List.columns.length + 10,
+			name: name.value,
+			tickets: [],
+			position: List.columns.length,
+			status: 'Created',
+			expanded: true,
+		};
+
+		// console.log({ List, column });&
+
+		setList({ columns: [...List.columns, column] });
+	};
+
+	const changeColumn = (ticketId, columnId, action) => {
+		const column = List.columns.find(column => column.id === columnId);
+		const ticket = column.tickets.find((ticket) => ticket.id === ticketId);
+
+		const targetPos = action === "+" ? column.position + 1 : column.position -1
+		const targetColumn = List.columns.find($column => $column.position === targetPos)
+		if (!targetColumn) return false
+
+		column.tickets = column.tickets.filter($ticket => $ticket.id !== ticketId)
+		targetColumn.tickets.push(ticket)
+
+		setList({ columns: List.columns });
+	};
+
+	const toggleColumnDisplay = (columnId) => {
+		const column = List.columns.find(column => column.id === columnId)
+		if (!column) return swalError('Column not found') 
+
+		column.expanded = !column.expanded
+
+		setList({ columns: List.columns })
+	};
+
+	//? ------------------ TICKETS ------------------
+	const createTicket = async (column) => {
+		// return swalInput({})
+		const title = await swal.fire({
+			title: 'Create ticket',
+			input: 'text',
+			inputValue: '',
+			inputPlaceholder: 'Title',
+			showCancelButton: true,
+			confirmButtonText: '➡️',
+			preConfirm: (title) => {
+				if (title === '') {
+					swal.showValidationMessage(`Title can't be empty`);
+				}
+				return title;
+			},
+		});
+		if (!title.isConfirmed) return;
+
+		const desc = await swal.fire({
+			title: 'Create ticket',
+			input: 'text',
+			inputValue: '',
+			inputPlaceholder: 'Description',
+			showCancelButton: true,
+			confirmButtonText: 'Create',
+			preConfirm: (desc) => {
+				if (desc === '') {
+					swal.showValidationMessage(`Description can't be empty`);
+				}
+				return desc;
+			},
+		});
+		if (!desc.isConfirmed) return;
+
+		const ticket = {
+			id: Math.floor(Math.random() * 1000),
+			title: title.value,
+			desc: desc.value,
+			status: 'Created',
+		};
+
+		column.tickets.push(ticket);
+		setList({ columns: List.columns });
+	};
 
 	const editTicket = (ticketId) => {
-		const row = List.rows.find(row => row.tickets.find(ticket => ticket.id === ticketId))
-		const ticket = row.tickets.find(ticket => ticket.id === ticketId)
+		const column = List.columns.find((column) =>
+			column.tickets.find((ticket) => ticket.id === ticketId)
+		);
+		const ticket = column.tickets.find((ticket) => ticket.id === ticketId);
 
 		swal.fire({
 			title: 'Edit ticket',
@@ -85,230 +218,268 @@ const TodoList = () => {
 			denyButtonText: 'Delete',
 			preConfirm: (desc) => {
 				try {
-					ticket.desc = desc
-					setList({ rows: List.rows })
+					ticket.desc = desc;
+					setList({ columns: List.columns });
 					swal.fire({
 						icon: 'success',
 						title: 'Saved!',
-					})
-				} catch(err) {
-					swal.showValidationMessage(
-						`Request failed: ${err}`
-					)
+					});
+				} catch (err) {
+					swal.showValidationMessage(`Request failed: ${err}`);
 				}
-			}
-		}).then(res => {
+			},
+		}).then((res) => {
 			if (res.isDenied) {
 				try {
-					row.tickets = row.tickets.filter(ticket => ticket.id !== ticketId)
-					setList({ rows: List.rows })
-	
+					column.tickets = column.tickets.filter(
+						(ticket) => ticket.id !== ticketId
+					);
+					setList({ columns: List.columns });
+
 					swal.fire({
 						icon: 'success',
 						title: 'Deleted !',
-					})	
-				} catch(err) {
+					});
+				} catch (err) {
 					swal.fire({
 						icon: 'error',
 						title: 'Oops...',
 						text: 'Something went wrong!',
-					})
+					});
 				}
 			}
-		})
-	}
+		});
+	};
 
-	const createRow = async () => {
+	const updateTicketPosition = (datas) => {
+		try {
+			// console.log(datas);
+			const {
+				source,
+				destination,
+				draggableId,
+				reason
+			} = datas
 
-		const name = await swal.fire({
-			title: 'Create new row',
-			input: 'text',
-			inputValue: 'New row',
-			inputPlaceholder: '',
-			showCancelButton: true,
-			confirmButtonText: 'Create',
-			preConfirm: (name) => {
-				if (name === '') {
-					swal.showValidationMessage(
-						`Please enter a name`
-					)
-				}
-				return name
-			}
+			//? If the item is dropped outside the list, return 
+			if (!destination) return false
 
-		})
-		if (!name.isConfirmed) return
+			//? If the reason is not drop, return
+			if (reason !== 'DROP') return false
 
-		const row = {
-			name: name.value,
-			tickets: [],
-			position: List.rows.length,
-			status: "Created"
-		}
+			//? If the destination is the same as the source, return
+			if (source.droppableId === destination.droppableId) return false
 
-		console.log({ List, row })
+			//? Find the column
+			const origin = List.columns.find(column => column.id.toString() === source.droppableId.toString())
+			const target = List.columns.find(column => column.id.toString() === destination.droppableId.toString())
 
-		setList({ rows: [...List.rows, row] })
-	}
+			//? Find the ticket in the origin
+			const ticket = origin.tickets.find(ticket => ticket.id.toString() === draggableId.toString())
 
-	const createTicket = async (row) => {
+			//? Remove it then push it to the target
+			origin.tickets = origin.tickets.filter(ticket => ticket.id.toString() !== draggableId.toString())
+			target.tickets.push(ticket)
 
-		const title = await swal.fire({
-			title: 'Create ticket',
-			input: 'text',
-			inputValue: '',
-			inputPlaceholder: 'Title',
-			showCancelButton: true,
-			confirmButtonText: '➡️',
-			preConfirm: (title) => {
-				if (title === '') {
-					swal.showValidationMessage(
-						`Title can't be empty`
-					)
-				}
-				return title
-			}
-		})
-		if (!title.isConfirmed) return
-
-
-		const desc = await swal.fire({
-			title: 'Create ticket',
-			input: 'text',
-			inputValue: '',
-			inputPlaceholder: 'Description',
-			showCancelButton: true,
-			confirmButtonText: 'Create',
-			preConfirm: (desc) => {
-				if (desc === '') {
-					swal.showValidationMessage(
-						`Description can't be empty`
-					)
-				}
-				return desc
-			}
-		})
-		if (!desc.isConfirmed) return
-		
-
-		const ticket = {
-			id: Math.floor(Math.random() * 1000),
-			title: title.value,
-			desc: desc.value,
-			status: "Created"
-		}
-
-		row.tickets.push(ticket)
-		setList({ rows: List.rows })
-	}
+			setList({ columns: List.columns })
+		} catch(err) { console.log(err); swalError(err) }
+	};
 
 	//#endregion
 
 	//#region --------------- DATA MAPPING ---------------
-	const getRows = () => {
+	const getColumns = () => {
 		try {
-
 			return (
-				<div className="rowsHolder">
-					{
-						List.rows.map(row => {	
+				<div className="columnsHolder">
+					{List.columns.map((column) => {
 						return (
-							
 							//? https://www.freecodecamp.org/news/how-to-add-drag-and-drop-in-react-with-react-beautiful-dnd/
-							<Droppable key={row.name} droppableId={row.position.toString()} index={row.position}>
+							<Droppable
+								key={ column.id }
+								index={ column.id }
+								droppableId={ column.id.toString() }
+							>
 								{(provided) => (
+									<div
+										className="column"
+										{...provided.droppableProps}
+										ref={provided.innerRef}
+									>
+										<div className="columnTitleHolder">
+											<h2 className="columnTitle">
+												{ column.name } ({ column.tickets.length })
+											</h2>
 
-									<div className='row' key={ row.position } { ...provided.droppableProps } ref={ provided.innerRef }>
-
-										<div className="rowTitleHolder">
-											<h2 className='rowTitle'> { row.name } </h2>
+											<button onClick={ () => toggleColumnDisplay(column.id) }> { column.expanded ? '-' : '+' } </button>
 										</div>
 
-										{ getTickets(row) }
+										
+										{ column.expanded && getTickets(column) }
 
-										{ getNewTicketButton(row) }
+										{ column.expanded && getNewTicketButton(column) }
 
-										{ provided.placeholder }
 									</div>
 								)}
 							</Droppable>
-					
-						)
-					})
-				}
+						);
+					})}
 
-					<div className="row new-row">
-						<div className="rowTitleHolder">
-							<h2 className='rowTitle'> New row </h2>
+					<div className="column new-column">
+						<div className="columnTitleHolder">
+							<h2 className="columnTitle"> New column </h2>
 						</div>
 
-						{ getNewRowButton() }
+						{getNewColumnButton()}
 					</div>
 				</div>
-			)
+			);
+		} catch (err) {
+			console.log({ err });
+			return <h1> Error getting columns </h1>;
+		}
+	};
 
-		} catch(err) { console.log({ err }); return <h1> Error getting rows </h1>}
-	}
-
-	const getTickets = (row) => {
+	const getTickets = (column) => {
 		try {
-
-			if (row.tickets.length >= 1) {
-				return row.tickets.map((ticket, i) => {
+			if (column.tickets.length >= 1) {
+				return column.tickets.map((ticket, i) => {
 					return (
 						//? https://www.freecodecamp.org/news/how-to-add-drag-and-drop-in-react-with-react-beautiful-dnd/
-						<Draggable key={ ticket.id } draggableId={ ticket.id.toString() } index={ i }>
+						<Draggable
+							key={ ticket.id }
+							draggableId={ ticket.id.toString() }
+							index={ i }
+						>
 							{(provided) => (
+								<div
+									className={`ticket ${
+										i === column.tickets.length - 1
+											? ' last-ticket'
+											: ''
+									}`}
+									ref={provided.innerRef}
+									{...provided.draggableProps}
+									{...provided.dragHandleProps}
+								>
+									<h3> {ticket.title} </h3>
+									<p> {ticket.desc} </p>
+									<div>
+										<button onClick={() => editTicket(ticket.id) } >
+											Edit{' '}
+										</button>
 
-							<div className={ `ticket ${ i === row.tickets.length - 1 ? ' last-ticket': '' }` } 
-								ref={ provided.innerRef } { ...provided.draggableProps } { ...provided.dragHandleProps }>
+										
+										<button onClick={() => changeColumn(ticket.id, column.id, '-') } >
+											⬅️{' '}
+										</button>
 
-								<h3> { ticket.title } </h3>
-								<p> { ticket.desc } </p>
-								<div>
-									<button onClick={ () => editTicket(ticket.id) }> Edit </button>
-									<button onClick={ () => changeRow(ticket.id, row.position, "-") }> ⬅️ </button>
-									<button onClick={ () => changeRow(ticket.id, row.position, "+") }> ➡️ </button>
+										<button onClick={() => changeColumn(ticket.id, column.id, '+') } >
+											➡️{' '}
+										</button>
+									</div>
 								</div>
-							</div>
 							)}
 						</Draggable>
-					)
-				})
+					);
+				});
 			} else {
-				return <h3 className='no-ticket'> No tickets </h3>
+				return <h3 className="no-ticket"> No tickets </h3>;
 			}
+		} catch (err) {
+			console.log({ err });
+			// return <h1> Error getting tickets </h1>;
+		}
+	};
 
-		} catch(err) { return <h1> Error getting tickets </h1>}
-	}
-
-	const getNewTicketButton = (row) => {
+	const getNewTicketButton = (column) => {
 		return (
-			<div onClick={ () => createTicket(row) } className="ticket new-ticket"> + </div>
-		)
-	}
+			<div
+				onClick={() => createTicket(column)}
+				className="ticket new-ticket"
+			>
+				{' '}
+				+{' '}
+			</div>
+		);
+	};
 
-	const getNewRowButton = (row) => {
+	const getNewColumnButton = (column) => {
 		return (
-			<div onClick={ () => createRow(row) } className="ticket new-ticket"> + </div>
-		)
-	}
+			<div onClick={() => createColumn(column)} className="ticket new-ticket">
+				{' '}
+				+{' '}
+			</div>
+		);
+	};
 	//#endregion
-
 
 	return (
 		<div>
 			<h1> TODO List </h1>
 
-			<DragDropContext onDragEnd={ ($event) => logStuff($event) }>
-				{ getRows(List) }				
+			<DragDropContext onDragEnd={($event) => updateTicketPosition($event)}>
+				{ getColumns(List) }
 			</DragDropContext>
 		</div>
-	)
+	);
+};
+
+//#region --------------- SWAL ---------------
+
+const swalInput = ({ title = "Input", text = null, input = "text", placeHolder = "", value = "", buttons = {} }) => {
+
+	const {
+		confirm = "Save",
+		cancel = "Cancel",
+		deny = "Deny",
+
+		showCancelButton = false,
+		showDenyButton = false,
+	} = buttons
+
+	return swal.fire({
+		title,
+		text,
+
+		input,
+		inputValue: value,
+		inputPlaceholder: placeHolder,
+
+		showCancelButton,
+		showDenyButton,
+
+		confirmButtonText: confirm,
+		cancelButtonText: cancel,
+		dentButtonText: deny,
+
+		preConfirm: (value) => {
+			console.log({ value })
+		}
+	})
 }
 
-const logStuff = (stuff) => {
-	console.log({ stuff })
+const swalError = (err) => {
+	return swal.fire({
+		icon: 'error',
+		title: 'Une erreur est survenue',
+		text: err
+	})
 }
+
+const swalSuccess = (title = "Succès !") => {
+	return swal.fire({
+		icon: 'success',
+		title
+	})
+}
+//#endregion
+
+//#region --------------- TOOLS ---------------
+const logStuff = (stuff) => {
+	console.log({ stuff });
+};
+//#endregion
+
 
 export default TodoList;
